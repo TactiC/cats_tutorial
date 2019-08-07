de6d0c6b513c20eea085ec71716064609a6dfcdb
ronalddehaan <rodhaan@wehkamp.nl>
2019-08-07 09:03:10 +0200

    Add PrintableSyntax and some tests

--- src/main/scala/nl/ronalddehaan/chapter_01/Printable.scala
+++ src/main/scala/nl/ronalddehaan/chapter_01/Printable.scala
@@ -17,4 +17,25 @@ object PrintableInstances {
 object Printable {
   def format[A](value: A)(implicit printer: Printable[A]): String = printer.format(value)
   def print[A](value: A)(implicit printer: Printable[A]): Unit = println(format(value))
+
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit print: Printable[A]): String = print.format(value)
+    def print(implicit print: Printable[A]): Unit = println(print.format(value))
+  }
+}
+
+object PrintableDemo extends App {
+  import PrintableInstances._
+  import PrintableSyntax._
+
+  val cat = Cat("Felix", 42, "black and white")
+
+  // print using Printable
+  Printable.print(cat)
+
+  // print using PrintableSyntax
+  cat.print
 }
