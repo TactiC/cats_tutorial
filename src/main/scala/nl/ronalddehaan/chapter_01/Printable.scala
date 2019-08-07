eb451b5176d7e7197a35ca8abec369f97714c2dc
ronalddehaan <rodhaan@wehkamp.nl>
2019-08-07 09:02:54 +0200

    Add a cat printer

--- src/main/scala/nl/ronalddehaan/chapter_01/Printable.scala
+++ src/main/scala/nl/ronalddehaan/chapter_01/Printable.scala
@@ -4,9 +4,14 @@ trait Printable[A] {
   def format(value: A): String
 }
 
+final case class Cat(name: String, age: Int, color: String)
+
 object PrintableInstances {
   implicit val stringPrinter: Printable[String] = (s: String) => s
   implicit val intPrinter: Printable[Int] = (i: Int) => i.toString
+
+  implicit val catPrinter: Printable[Cat] = (c: Cat) =>
+    s"${Printable.format(c.name)} is a ${Printable.format(c.age)} year old ${Printable.format(c.color)} cat."
 }
 
 object Printable {
